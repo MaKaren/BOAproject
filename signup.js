361db5d6b86ab13478657e056003f85b3f009db2
Axurj <acerj2468@gmail.com>
2020-07-20 16:46:16 -0700

    Got started on the js + added new p tag

--- signup.js
+++ signup.js
@@ -4,3 +4,43 @@
     Team Name: MKJ
     Theme: Peaches
 */
+
+let formRecieved = document.getElementById("inputForm");
+let userRecieved = document.getElementById("myUsername");
+let passRecieved = document.getElementById("myPassword");
+let confirmRecieved = document.getElementById("myConfirmPassword");
+let emailRecieved = document.getElementById("myEmail");
+
+// let getUser = document.querySelector("#myUsername");
+// let message = document.querySelector("#userTag");
+
+//function validate(event) {
+//    let inputVar = event.target.value;
+//    console.log("inputVar", inputVar);
+//    if(inputVar.length < 7) {
+//console.log("invalid input");
+ //   } else {
+//        message.setAttribute("class", "validUser");
+ //   }
+//}
+
+ // getUser.addEventListener("change", validate);
+
+ 
+
+formRecieved.addEventListener("submit", makeAccount);
+
+function makeAccount(event) {
+    event.preventDefault();
+
+    // if(userRecieved.value >= 8 && userRecieved.value <= 20){
+            
+    // }
+
+    console.log("userRecieved", userRecieved.value);
+    console.log("passRecieved", passRecieved.value);
+    console.log("confirmRecieved", confirmRecieved.value);
+    console.log("emailRecieved", emailRecieved.value);
+}
+
+// &check; makes a checkmark
